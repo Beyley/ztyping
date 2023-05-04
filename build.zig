d5ae5f6ad942abc3785404c64c94e6af86611fa8
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-05-04 15:36:59 -0700

    Get wgpu building for windows

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const zaudio = @import("libs/zaudio/build.zig");
 const sdl = @import("libs/SDL/build.zig");
 const fontstash = @import("libs/fontstash/build.zig");
+const wgpu = @import("wgpu.zig");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -39,51 +40,14 @@ pub fn build(b: *std.Build) !void {
     } //SDL
 
     { //wgpu
-        const wgpu_native_path = root_path ++ "libs/wgpu-native/";
+        exe.addObjectFile(try wgpu.create_wgpu(b, target, optimize));
 
-        //Get the users name
-        var username = std.os.getenv("USER") orelse return error.UserVariableNotSet;
-
-        //TODO: install the proper toolchains
-
-        //Get the possible path to `cross`
-        //TODO: make this logic much smarter
-        var cross_path = try std.mem.concat(b.allocator, u8, &.{ "/home/", username, "/.cargo/bin/cross" });
-
-        //Array list will store our target name
-        var cross_target = std.ArrayList(u8).init(b.allocator);
-
-        //Add cpu arch
-        try cross_target.appendSlice(@tagName(target.getCpuArch()));
-        try cross_target.append('-');
-        //Add special thing
-        switch (target.getOsTag()) {
-            .windows => try cross_target.appendSlice("pc"),
-            .linux => try cross_target.appendSlice("unknown"),
-            else => {
-                //if theres no special thing, pop the last hyphen
-                _ = cross_target.pop();
-            },
+        if (target.getOsTag() == .windows) {
+            exe.linkSystemLibrary("ws2_32");
+            exe.linkSystemLibrary("userenv");
+            exe.linkSystemLibrary("bcrypt");
+            exe.linkSystemLibrary("d3dcompiler_47");
         }
-        try cross_target.append('-');
-        //Add OS
-        try cross_target.appendSlice(@tagName(target.getOsTag()));
-        try cross_target.append('-');
-        //Add ABI
-        try cross_target.appendSlice(@tagName(target.getAbi()));
-
-        var build_result = try std.ChildProcess.exec(.{
-            .allocator = b.allocator,
-            .argv = &.{ cross_path, "build", "--target", cross_target.items, if (optimize == std.builtin.OptimizeMode.Debug) "" else "--release" },
-            .cwd = wgpu_native_path,
-        });
-        defer b.allocator.free(build_result.stdout);
-        defer b.allocator.free(build_result.stderr);
-
-        // std.debug.print("cross output: {s}\n", .{build_result.stderr});
-
-        exe.addLibraryPath(try std.mem.concat(b.allocator, u8, &.{ root_path, "libs/wgpu-native/target/", cross_target.items, "/", if (optimize == std.builtin.OptimizeMode.Debug) "debug" else "release" }));
-        exe.linkSystemLibrary("wgpu_native");
 
         exe.addIncludePath("libs/wgpu-native/ffi");
     } //wgpu
---
+++ wgpu.zig
@@ -0,0 +1,75 @@
+const std = @import("std");
+
+///Tries to compile and prepare wgpu, returning the path to a static library to add with addObjectFile()
+pub fn create_wgpu(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode) ![]const u8 {
+    const wgpu_native_path = root_path ++ "libs/wgpu-native/";
+
+    //Get the users name
+    var username = std.os.getenv("USER") orelse return error.UserVariableNotSet;
+
+    //TODO: install the proper toolchains
+
+    //Get the possible path to `cross`
+    //TODO: make this logic much smarter
+    var cross_path = try std.mem.concat(b.allocator, u8, &.{ "/home/", username, "/.cargo/bin/cross" });
+
+    //Array list will store our target name
+    var cross_target = std.ArrayList(u8).init(b.allocator);
+
+    //Add cpu arch
+    try cross_target.appendSlice(@tagName(target.getCpuArch()));
+    try cross_target.append('-');
+    //Add special thing
+    switch (target.getOsTag()) {
+        .windows => try cross_target.appendSlice("pc"),
+        .linux => try cross_target.appendSlice("unknown"),
+        else => {
+            //if theres no special thing, pop the last hyphen
+            _ = cross_target.pop();
+        },
+    }
+    try cross_target.append('-');
+    //Add OS
+    try cross_target.appendSlice(@tagName(target.getOsTag()));
+    try cross_target.append('-');
+    //Add ABI
+    try cross_target.appendSlice(@tagName(target.getAbi()));
+
+    var args = std.ArrayList([]const u8).init(b.allocator);
+    defer args.deinit();
+
+    var env_map = try std.process.getEnvMap(b.allocator);
+    // try env_map.put("CROSS_CONTAINER_ENGINE_NO_BUILDKIT", "1");
+
+    try args.append(cross_path);
+    try args.append("build");
+    try args.append("--target");
+    try args.append(cross_target.items);
+    if (optimize != .Debug) {
+        try args.append("--release");
+    }
+
+    var build_result = try std.ChildProcess.exec(.{
+        .allocator = b.allocator,
+        .argv = args.items,
+        .cwd = wgpu_native_path,
+        .env_map = &env_map,
+    });
+    defer b.allocator.free(build_result.stdout);
+    defer b.allocator.free(build_result.stderr);
+
+    if (build_result.term.Exited != 0) {
+        std.debug.print("exitied with code: {d}\n{s}\n", .{ build_result.term.Exited, build_result.stderr });
+        return error.UnableToCompileWgpu;
+    }
+
+    var target_path = try std.mem.concat(b.allocator, u8, &.{ root_path, "libs/wgpu-native/target/", cross_target.items, "/", if (optimize == std.builtin.OptimizeMode.Debug) "debug" else "release" });
+
+    return (try std.mem.concat(b.allocator, u8, &.{ target_path, "/libwgpu_native.a" }));
+}
+
+fn root() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse ".";
+}
+
+const root_path = root() ++ "/";
