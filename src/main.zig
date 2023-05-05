2b09c4b038393d291d1b583e0bfa5795a4d2d52b
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-05-04 20:35:27 -0700

    comments

--- src/main.zig
+++ src/main.zig
@@ -26,6 +26,7 @@ pub fn main() !void {
     const engine = try zaudio.Engine.create(null);
     defer engine.destroy();
 
+    //Initialize SDL
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
         std.debug.print("SDL init failed! err:{s}\n", .{c.SDL_GetError()});
         return error.InitSdlFailure;
@@ -33,6 +34,7 @@ pub fn main() !void {
     defer c.SDL_Quit();
     std.debug.print("Initialized SDL\n", .{});
 
+    //Create the window
     const window = c.SDL_CreateWindow("ztyping", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 640, 480, c.SDL_WINDOW_SHOWN) orelse {
         std.debug.print("SDL window creation failed! err:{s}\n", .{c.SDL_GetError()});
         return error.CreateWindowFailure;
@@ -40,11 +42,13 @@ pub fn main() !void {
     defer c.SDL_DestroyWindow(window);
     std.debug.print("Created SDL window\n", .{});
 
+    //Create the webgpu instance
     var instance = try gfx.create_instance();
     defer c.wgpuInstanceDrop(instance);
 
     std.debug.print("got instance 0x{x}\n", .{@ptrToInt(instance.?)});
 
+    //Create the webgpu surface
     var surface = try gfx.create_surface(instance, window);
     defer c.wgpuSurfaceDrop(surface);
 
