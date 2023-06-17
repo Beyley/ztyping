e82d30c2624891a8a5f54f7554d88957808780a4
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-06-17 14:48:19 -0700

    convert: Add safety checks for convert loading

--- src/convert.zig
+++ src/convert.zig
@@ -5,13 +5,17 @@ const Self = @This();
 
 pub const Conversion = struct {
     allocator: std.mem.Allocator,
-    hiragana: []const u8,
-    romaji: []const u8,
+    hiragana: [:0]const u8,
+    romaji: [:0]const u8,
+    end_cut: ?[:0]const u8,
     length: usize,
 
     pub fn deinit(self: Conversion) void {
         self.allocator.free(self.hiragana);
         self.allocator.free(self.romaji);
+        if (self.end_cut) |end_cut| {
+            self.allocator.free(end_cut);
+        }
     }
 };
 
@@ -39,8 +43,25 @@ pub fn readUTypingConversions(allocator: std.mem.Allocator) !Self {
     var iconv = IConv.ziconv_open("Shift_JIS", "UTF-8");
     defer IConv.ziconv_close(iconv);
 
-    var data = try iconv.convert(allocator, raw_data);
-    defer allocator.free(data);
+    //Do the initial conversion from Shift_JIS to UTF-8
+    var converted_data = try iconv.convert(allocator, raw_data);
+    defer allocator.free(converted_data);
+
+    //Allocate a staging buffer to do the replacements in
+    var raw_replaced_data = try allocator.alloc(u8, converted_data.len);
+    defer allocator.free(raw_replaced_data);
+
+    //NOTE: this is safe as the tilde and backslash are both 1 bytes long, while the overline and yen sign are both longer, so the data is always guarenteed to be shoretr
+    //Replace the tilde with the overline
+    const underlines_replaced = std.mem.replace(u8, converted_data, "‾", "~", raw_replaced_data);
+    //Replace the yen sign with the backslash
+    const yen_replaced = std.mem.replace(u8, raw_replaced_data[0..(raw_replaced_data.len - underlines_replaced * ("‾".len - "~".len))], "¥", "\\", converted_data);
+
+    //Calculate the length to cut off the end of the file, as the replacements will have shortened it
+    var length_to_cut = underlines_replaced * ("‾".len - "~".len) + yen_replaced * ("¥".len - "\\".len);
+
+    //Cut off the end of the file
+    var data = converted_data[0..(converted_data.len - length_to_cut)];
 
     var conversions = std.ArrayList(Conversion).init(allocator);
     errdefer {
@@ -54,9 +75,9 @@ pub fn readUTypingConversions(allocator: std.mem.Allocator) !Self {
     var stream = std.io.fixedBufferStream(data);
     var reader = stream.reader();
 
+    var buf: [50]u8 = undefined;
     while (true) {
-        var raw_line = try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 50) orelse break;
-        defer allocator.free(raw_line);
+        var raw_line = try reader.readUntilDelimiterOrEof(&buf, '\n') orelse break;
 
         var line = if (raw_line[raw_line.len - 1] == '\r') raw_line[0..(raw_line.len - 1)] else raw_line;
 
@@ -65,13 +86,39 @@ pub fn readUTypingConversions(allocator: std.mem.Allocator) !Self {
 
         var romaji = line[0..idx1];
         var hiragana = line[(idx1 + 1)..(idx2 orelse line.len)];
-        var end_cut = if (idx2 == null) "" else line[(idx2.? + 1)..];
+        var end_cut: ?[]const u8 = if (idx2 == null) null else line[(idx2.? + 1)..];
+
+        //The end cut must not be empty, so if it is, set it to null
+        if (end_cut != null and end_cut.?.len == 0) {
+            end_cut = null;
+        }
+
+        //The romaji must be at least 1 char long
+        if (romaji.len == 0) {
+            return error.InvalidConvertFile;
+        }
+
+        //The hiragana must be at least 1 char long
+        if (hiragana.len == 0) {
+            return error.InvalidConvertFile;
+        }
+
+        //If the end cut is present, the romaji must not be only 1 char long
+        if (end_cut != null and romaji.len == 1) {
+            return error.InvalidConvertFile;
+        }
+
+        //If the end cut is present, it must be 1 char long
+        if (end_cut != null and end_cut.?.len != 1) {
+            return error.InvalidConvertFile;
+        }
 
         try conversions.append(.{
             .allocator = allocator,
-            .hiragana = try allocator.dupe(u8, hiragana),
-            .romaji = try allocator.dupe(u8, romaji),
-            .length = romaji.len - end_cut.len,
+            .hiragana = try allocator.dupeZ(u8, hiragana),
+            .romaji = try allocator.dupeZ(u8, romaji),
+            .end_cut = if (end_cut == null) null else try allocator.dupeZ(u8, end_cut.?),
+            .length = if (end_cut == null) romaji.len else romaji.len - end_cut.?.len,
         });
     }
 
